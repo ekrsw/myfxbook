1f7227574d18991fe76390141a65ee000f4439ef
ekrsw <eisuke.koresawa@gmail.com>
2025-12-15 12:33:25 +0900

    Home py-8 -> py-4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Dashboard from '@/components/Dashboard';
 
 export default function Home() {
   return (
-    <main className="min-h-screen gb-gray-50 py-8 px-4">
+    <main className="min-h-screen gb-gray-50 py-4 px-4">
         <div className="max-w-4xl mx-auto">
             <Dashboard />
         </div>
